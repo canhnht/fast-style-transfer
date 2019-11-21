2b3932c0df1a33b110357e54008a91169bd07827
ACworks-AI-BlueNguyen <thanhcanh@acworks.co.jp>
2019-11-21 17:58:31 +0900

    Add script

---
+++ test-image.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+#python evaluate.py --checkpoint models/wave.ckpt \
+#  --in-path examples/my-content/stata.jpg \
+#  --out-path examples/my-result/stata_output.jpg \
+#  --device /gpu:0
+
+python evaluate.py --checkpoint models/wave.ckpt --in-path examples/my-content/chicago.jpg --out-path examples/my-result/chicago_output.jpg
+
